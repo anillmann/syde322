4f8fa6b86bab1de3cf37d7f671d6e32a498c65fd
melissalynett1 <melissa.a.lynett@gmail.com>
2016-02-02 14:15:04 -0500

    code

--- a2/json-page-dynamic.js
+++ a2/json-page-dynamic.js
@@ -1,31 +1,48 @@
 $(document).ready(function(){
 
-	$("#parameter_form").submit(function(event){
-		var ingred_string = $("#ingred").val();
-		var query_string = $("#query").val();
-		var api_string = "http://recipepuppy.com/api/?i="+ ingred_string + "&q=" + query_string;
+	// $("#parameter_form").submit(function(event){
+	// 	var ingred_string = $("#ingred").val();
+	// 	var query_string = $("#query").val();
+	// 	var api_string = "http://recipepuppy.com/api/?i="+ ingred_string + "&q=" + query_string;
 
-		$.ajax({url: "http://recipepuppy.com/api/", data:{i:"",q:""}, type: 'GET', jsonpCallback : 'onApiResponse' });
-
-		function onApiResponse(data) {
+		$.getJSON("http://recipepuppy.com/api/?i=onions,garlic&q=omelet", function(data) {
 			var i = 0, dataSize = data.results.length, opt_string = '';
 	    	for(i; i < dataSize; i++){
 	    		var row = i+1;
 	       		$('#main_table tr:eq('+ row +') td:eq(0)').html(data.results[i].title);
 	       		$('#main_table tr:eq('+ row +') td:eq(1)').html(data.results[i].href);
 	       		$('#main_table tr:eq('+ row +') td:eq(2)').html(data.results[i].ingredients);
-	     		}			
-		}
+	     		}
+		})
 
-		// $.getJSON(api_string, function(data) {
+		// $.ajax({
+		// 	url: api_string, 
+		// 	// data: {
+		// 	// 	i:ingred_string,
+		// 	// 	q:query_string
+		// 	// }, 
+		// 	// type: 'GET', 
+		// 	// dataType: 'jsonp',
+		// 	// error: function(data) {
+		// 	// 	alert(data);
+		// 	// },
+		// 	// success: function(data) {
+		// 	// 	alert(data);
+		// 	// },
+		// 	jsonpCallback : 'onApiResponse' 
+		// });
+
+		// function onApiResponse(data) {
 		// 	var i = 0, dataSize = data.results.length, opt_string = '';
 	 //    	for(i; i < dataSize; i++){
 	 //    		var row = i+1;
 	 //       		$('#main_table tr:eq('+ row +') td:eq(0)').html(data.results[i].title);
 	 //       		$('#main_table tr:eq('+ row +') td:eq(1)').html(data.results[i].href);
 	 //       		$('#main_table tr:eq('+ row +') td:eq(2)').html(data.results[i].ingredients);
-	 //     		}
-		// })
+	 //     		}			
+		// }
 
-	});
+
+
+	// });
 });
